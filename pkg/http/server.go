cd41b827853feb136db27af734972a94be1f95ee
rodblg <rodrigoblancas19@gmail.com>
2024-03-12 20:43:49 -0600

    chore: Add "running" log into the ListenAndServe() function

--- pkg/http/server.go
+++ pkg/http/server.go
@@ -1,15 +1,21 @@
 package http
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 )
 
 func ListenAndServe() {
+
+	log.Println("running server...")
 	r := chi.NewRouter()
 
 	r.Mount("/urls", NewUrlController().Routes())
 
-	http.ListenAndServe(":8080", r)
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		log.Println("error initializing server...")
+		return
+	}
 }

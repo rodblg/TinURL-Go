d4d8ec1aaf605125c83ecc9f592c9351efdd84a4
rodblg <rodrigoblancas19@gmail.com>
2024-03-12 20:48:42 -0600

    chore:Call hash pkg to generate a new hash map

--- pkg/http/url.go
+++ pkg/http/url.go
@@ -1,9 +1,11 @@
 package http
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	HashMap "github.com/rodblg/TinURL-Go/pkg/hash"
 )
 
 type Url struct {
@@ -16,7 +18,7 @@ func NewUrlController() *Url {
 func (c *Url) Routes() chi.Router {
 	r := chi.NewRouter()
 
-	r.Post("/", c.GetTinyUrl)
+	r.Get("/tinyUrl", c.GetTinyUrl)
 	r.Post("/", c.NumberClicks)
 
 	return r
@@ -24,6 +26,16 @@ func (c *Url) Routes() chi.Router {
 
 func (c *Url) GetTinyUrl(w http.ResponseWriter, r *http.Request) {
 
+	a := HashMap.NewMap()
+	log.Print(a)
+
+	//GET ORIGINAL URL AS STRING
+
+	//CREATE HASH TABLE BUT YOU MUST AVOID COLLISION OF THE HASH MAP
+
+	//CALL HASH FUNCTION
+
+	//GENERATE NEW URL
 }
 
 func (c *Url) NumberClicks(w http.ResponseWriter, r *http.Request) {
